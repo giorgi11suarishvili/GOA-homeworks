38d35c2f6d359dd893fe5e699e47d8a8ae60305b
giorgi11suarishvili <166071720+giorgi11suarishvili@users.noreply.github.com>
2024-04-06 13:42:25 -0700

    Add files via upload

---
+++ DAY1/homework/homework.py
@@ -0,0 +1,10 @@
+name = "giorgi"
+surname= "suarishvili"
+age= "age 17"
+height= " heigh 189"
+knows_programing=True
+live="live in tbilisi"
+profession=" profession cooker"
+studying="start studying programming at Goal-Oriented Academy ^_^"
+
+print(name + " " + surname + " " + str(age) + " " + height + " " + profession + " " + live + " " + studying )
